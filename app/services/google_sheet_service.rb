29310157020e2b2e44a0d9316d4273b753136904
CuongPham <harry88pham@gmail.com>
2022-12-28 13:13:01 +0700

    fix

--- app/services/google_sheet_service.rb
+++
@@ -1,9 +0,0 @@
-require "google_drive"
-class GoogleSheetService 
-  def initialize(title)
-    session = GoogleDrive::Session.from_service_account_key(ENV['GOOGLE_APPLICATION_CREDENTIALS'])
-    spreadsheet = session.spreadsheet_by_title(title)
-    worksheet = spreadsheet.worksheets.first
-    return worksheet
-  end
-end
