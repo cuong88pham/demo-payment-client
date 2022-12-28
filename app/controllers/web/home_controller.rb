54da5c82c38177782a336390203d19884c0722a0
CuongPham <harry88pham@gmail.com>
2022-12-28 23:22:37 +0700

    fix

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -8,7 +8,7 @@ module Web
     def webhook 
       data = params["data"]
       tx = UserTransaction.find_by(tracking_id: data["tracking_id"])
-      tx.paid? if tx && tx.amount.to_f == data["amount"].to_f 
+      tx.paid! if tx && tx.amount.to_f == data["amount"].to_f 
       render json: {}, status: 200
     end
 
